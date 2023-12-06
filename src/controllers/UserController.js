e2c3f040f3ca6687354dc8d9cf89a48b42d84760
AjayArcher23 <ajayarcherr@gmail.com>
2023-12-06 16:16:02 +1100

    new get request for all users, seed file for database.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,7 +4,15 @@ const { User } = require('../models/UserModel');
 
 
 // make an instance of a Router
-const router = express.Router(); 
+const router = express.Router();  
+
+// GET localhost:3000/users/
+router.get("/", async (request, response) => {
+	let result = await User.find({});
+
+	response.json({result});
+})
+
 
 // GET localhost:3000/users/id
 router.get("/:id", async (request, response) => {
@@ -21,4 +29,4 @@ router.post("/", async (request, response) => {
 	response.json(newUser);
 }); 
 
-module.exports = router ;
+module.exports = router ; 
--- src/models/ArtworkModel.js
+++ src/models/ArtworkModel.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const mongoose = require('mongoose');
 
-const ArtworkSchema = new mongoose.Schema({
-	user: {
-		type: ObjectId,
-		ref: 'User',
-		required: true,  
-        unique: false
-	}, 
-    uploadedPhoto: {
-        type: String, // URL of uploaded photo
-        required: true,   
-        unique: false
-    },
-	description: {
-		type: String,
-		trim: true,  
-        required: false,
-        unique: false
-	}, 
-    genre: {
-        type: String, 
-        required: true, 
-        unique: false, 
-        trim: true
-    }, 
-    medium: {
-        type: String, 
-        required: false, 
-        unique: false, 
-        trim: true
-    }
-}); 
 
-const Artwork = mongoose.model('Artwork', ArtworkSchema);
+// const ArtworkSchema = new mongoose.Schema({
+// 	user: {
+// 		type: ObjectId,
+// 		ref: 'User',
+// 		required: true,  
+//         unique: false
+// 	}, 
+//     uploadedPhoto: {
+//         type: String, // URL of uploaded photo
+//         required: true,   
+//         unique: false
+//     },
+// 	description: {
+// 		type: String,
+// 		trim: true,  
+//         required: false,
+//         unique: false
+// 	}, 
+//     genre: {
+//         type: String, 
+//         required: true, 
+//         unique: false, 
+//         trim: true
+//     }, 
+//     medium: {
+//         type: String, 
+//         required: false, 
+//         unique: false, 
+//         trim: true
+//     }
+// }); 
 
-module.exports = { Artwork }
+// const Artwork = mongoose.model('Artwork', ArtworkSchema);
+
+// module.exports = { Artwork }
