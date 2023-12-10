92a92c7e93f83e0377ed6fe109318fd252aa0fa5
AjayArcher23 <ajayarcherr@gmail.com>
2023-12-10 14:56:12 +1100

    user DELETE and PATCH methods

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -34,14 +34,12 @@ router.post("/", async (request, response) => {
 router.post("/login", async (request, response) => {
 	// Find user by provided email
 	let targetUser = await User.findOne({email: request.body.email}).catch(error => error); 
-
 	// Check if user provided the correct password
 	let isPasswordCorrect = await comparePassword(request.body.password, targetUser.password);  
 
 	if (!isPasswordCorrect){
 		response.status(403).json({error:"Password was incorrect"}); 
 	} 
-
 	// If they provided the correct, generate a JWT
 	let freshJwt = generateJwt(targetUser._id.toString());
 
@@ -50,8 +48,57 @@ router.post("/login", async (request, response) => {
 		jwt: freshJwt
 	});
 
-})
+}); 
 
+// DELETE localhost:3000/users/id
+router.delete("/:id", async (request, response) => {
+	const _id = request.params.id;
+  
+	try {
+	  const result = await User.findByIdAndDelete(_id);
+  
+	  if (result) {
+		response.json({
+		  message: "User deleted successfully",
+		  user: result,
+		});
+	  } else {
+		response.status(404).json({
+		  message: "User not found",
+		});
+	  }
+	} catch (error) {
+	  console.error(error);
+	  response.status(500).json({
+		message: "Internal Server Error",
+	  });
+	}
+  }); 
+
+
+// PATCH localhost:3000/users/id
+router.patch("/:id", async (request, response) => {
+	const _id = request.params.id;
+    const updatedUserData = request.body; 
+
+    try {
+        // Find the user by ID and update their data
+        const updatedUser = await User.findByIdAndUpdate(_id, updatedUserData, { new: true });
+
+        if (!updatedUser) {
+            return response.status(404).json({ error: "User not found" });
+        } else {
+			response.status(404).json({
+			  message: "Successfully updated user",
+			});
+		  }
+	} catch (error) {
+		  console.error(error);
+		  response.status(500).json({
+		  message: "Internal Server Error",
+		  });
+   	  }
+});
 
 module.exports = router;
 
